d2c0a0798df58f4cdd6afb8a9f7328e47b770173
RiceCake404 <etenaelta@outlook.com>
2021-11-10 12:05:57 +0800

    chore(init): add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
--- src/main/main.ts
+++ src/main/main.ts
@@ -1,35 +1,38 @@
 // main.js
 // Modules to control application life and create native browser window
 
-import { app, BrowserWindow } from "electron";
-import * as path from "path";
-import contextMenu from "electron-context-menu";
-contextMenu()
-let mainWindow
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import contextMenu from 'electron-context-menu';
+
+contextMenu();
+let mainWindow;
+
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
   // and load the index.html of the app.
   if (app.isPackaged) {
     mainWindow.loadFile(path.join(__dirname, `../render/index.html`));
   } else {
-    mainWindow.loadURL("http://localhost:8080");
+    mainWindow.loadURL('http://localhost:8080');
   }
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools();
 }
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(function () {
   createWindow();
-  app.on("activate", function () {
+  app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
@@ -38,9 +41,8 @@ app.whenReady().then(function () {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
 });
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
