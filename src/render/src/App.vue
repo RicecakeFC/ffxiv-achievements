43cb699d5a46e963909f73e1ff2da5df3233733d
RiceCake404 <etenaelta@outlook.com>
2021-11-10 14:20:51 +0800

    chore(init): set locale

--- src/render/src/App.vue
+++ src/render/src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <el-config-provider :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
+<script>
+import { defineComponent } from 'vue';
+import { ElConfigProvider } from 'element-plus';
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default defineComponent({
+  components: {
+    ElConfigProvider,
+  },
+  setup() {
+    return {
+      locale: zhCn,
+    };
+  },
+});
+</script>
