509df6596da03b05b1a635dbd2ee6329fb3c5014
RiceCake404 <etenaelta@outlook.com>
2021-11-10 14:10:15 +0800

    chore(init): add element plus and unplugin-vue-components

--- src/render/vue.config.js
+++ src/render/vue.config.js
@@ -1,9 +1,17 @@
 // vue.config.js
-
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
+const Components = require('unplugin-vue-components/webpack');
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
   outputDir: '../../dist/src/render',
-  publicPath: process.env.VUE_APP_PUBLIC_PATH
-}
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
+  configureWebpack: {
+    plugins: [
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
+};
