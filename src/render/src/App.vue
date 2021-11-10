6d048f6463bb5c347eb9772e33cc8ba911cbc8e0
RiceCake404 <etenaelta@outlook.com>
2021-11-11 00:11:47 +0800

    feat(top): update top page style

--- src/render/src/App.vue
+++ src/render/src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <el-config-provider :locale="locale">
     <el-container>
-      <el-container>
-        <el-header> header </el-header>
-        <el-row>
-          <top-nav />
-          <el-main>
-            <router-view />
-          </el-main>
+      <el-header
+          style="border-bottom: solid #d9d9d9 1px; position:fixed; z-index: 1; background-color: #ffffff; width: 100%">
+        <el-row style="height: inherit; display: flex; align-items: center">
+          <el-space>
+            <el-image
+                style="width: 40px; height: 40px"
+                :src="logo"
+                fit="contain"
+            ></el-image>
+            <div style="font-size: var(--el-font-size-extra-large)">金平糖</div>
+            <el-tag>Ver.0.0.1</el-tag>
+          </el-space>
         </el-row>
-        <el-footer> footer </el-footer>
+      </el-header>
+      <el-container style="margin-top: 60px; height: 100vh">
+        <el-aside width="64px" style="position:fixed;height: 100%">
+          <top-nav style="height: 100%"/>
+        </el-aside>
+        <el-container style="margin-left: 64px">
+          <el-main style="height: 2000px; background-color: #42b983">Main</el-main>
+          <el-footer height="32px">Footer</el-footer>
+        </el-container>
       </el-container>
     </el-container>
   </el-config-provider>
@@ -20,6 +33,7 @@ import { ElConfigProvider } from 'element-plus';
 
 import zhCn from 'element-plus/lib/locale/lang/zh-cn';
 import TopNav from '@/components/TopNav';
+import logo from '../../../assets/favicon.png'
 
 export default defineComponent({
   components: {
@@ -29,7 +43,14 @@ export default defineComponent({
   setup() {
     return {
       locale: zhCn,
+      logo: logo,
     };
   },
 });
 </script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
