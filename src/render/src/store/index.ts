f73187a81ec94ac41305efe08dbea0e767ac334e
RiceCake404 <etenaelta@outlook.com>
2021-11-12 17:03:52 +0800

    feat: persist states in local storage

--- src/render/src/store/index.ts
+++ src/render/src/store/index.ts
@@ -2,25 +2,37 @@
 import { InjectionKey } from 'vue';
 import { createStore, Store, useStore as baseUseStore } from 'vuex';
 import { UserConfig, UserConfigState } from '@/store/user-config';
+import VuexPersistence from 'vuex-persist';
 
-export interface State {
+export interface RootState {
   count: number;
   userConfig?: UserConfigState;
 }
 
-export const key: InjectionKey<Store<State>> = Symbol();
+const vuexLocalStorage = new VuexPersistence<RootState>({
+  key: 'rc-ffxiv-achievements-vuex',
+  storage: window.localStorage,
+  reducer: (state) => {
+    return {
+      userConfig: state.userConfig,
+    };
+  },
+});
 
-export const store = createStore<State>({
+export const key: InjectionKey<Store<RootState>> = Symbol();
+
+export const store = createStore<RootState>({
   state: {
     count: 0,
   },
   modules: {
     userConfig: UserConfig,
   },
+  plugins: [vuexLocalStorage.plugin],
 });
 
 // define your own `useStore` composition function
-export function useStore(): Store<State> {
+export function useStore(): Store<RootState> {
   return baseUseStore(key);
 }
 
